77673cdb197d2d98ae9f154d129b880de3755ce1
emkael <emkael@tlen.pl>
2013-09-14 16:36:31 +0200

    * przekierowanie na stronę projektu

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+header('HTTP/1.1 302');
+header('Location: https://github.com/emkael/osika');
+
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>OSiKa</title>
+    </head>
+    <body>
+        Przekierowanie na <a href="https://github.com/emkael/osika">stronę projektu w github.com</a>...
+    </body>
+</html>
