63e2294c0b4e03b281882435a63ee3b5a2d470b8
emkael <emkael@tlen.pl>
2013-09-14 23:20:13 +0200

    * zestaw testów dla kalkulatora (rozdania, którymi sprawdzana była stara wersja skryptu)

---
+++ tests/OsikaEvaluatorTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class OsikaEvaluatorTest extends PHPUnit_Framework_TestCase {
+
+  private $_evaluator;
+
+  public function setUp() {
+    require_once('../bin/lib/OsikaEvaluator.php');
+    $this->_evaluator = new OsikaEvaluator();
+  }
+
+  public function handProvider() {
+    $file = file('OsikaEvaluatorTest.tests');
+    $ret = [];
+    $switch = false;
+    $item;
+    foreach ($file as $line) {
+      if (trim($line)) {
+	if ($switch) {
+	  $item[] = json_decode($line);
+	  $ret[] = $item;
+	}
+	else {
+	  $item = [trim($line)];
+	}
+	$switch = !$switch;
+      }
+    }
+    return $ret;
+  }
+
+  /**
+   * @dataProvider handProvider
+   **/
+  public function testEvaluation($hand, $result) {
+    $this->_evaluator->setHand($hand);
+    $this->assertEquals($result, $this->_evaluator->evaluate());
+  }
+
+}
+
+?>
